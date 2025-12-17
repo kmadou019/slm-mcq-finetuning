cada7f90315d31852dd2f9595d68059ffbb43331
Madou KONE <kmadou019@gmail.com>
2025-12-17 16:50:25 +0100

    quality distrib / topic

--- scripts/build_data_set.sh
+++ scripts/build_data_set.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 cd ../data/lisa_sheets
-echo "folder,id,content_raw,rubric"
+echo "id,item"
 for dir in $(ls);
 do
     if test -d "${dir}"; then
         cd $dir
         for sheet in $(ls);
         do
-            rubrique=$(grep "Rubrique" $sheet)
-            rubrique=${rubrique:10}
-            echo $dir","${sheet::12}","\""$(cat $sheet)"\"","\"$rubrique\"
+            item=$(grep "Item_parent_short" $sheet)
+            item=${item:19}
+            echo ${sheet::7}","\"$item\"
             #sed -i 's/"/""/g' "$sheet"
+            break
         done
         cd ..
     fi
