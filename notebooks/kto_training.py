b318344fd814f0f791a7c236e98f05a2cd5ca534
Madou KONE <kmadou019@gmail.com>
2026-04-21 16:31:40 +0200

    update the code of kto_train

--- notebooks/kto_training.py
+++ notebooks/kto_training.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 from datasets import load_dataset
-from trl.experimental.kto import KTOConfig, KTOTrainer
+from trl import KTOConfig, KTOTrainer
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-model = AutoModelForCausalLM.from_pretrained("google/medgemma-4b-it")
-tokenizer = AutoTokenizer.from_pretrained("google/medgemma-4b-it")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-8B")
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-8B")
 train_dataset = load_dataset("json", data_files="../data/kto_dataset/train.jsonl", split="train").select(range(100))
 eval_dataset  = load_dataset("json", data_files="../data/kto_dataset/eval.jsonl",  split="train").select(range(20))
 
-training_args = KTOConfig(output_dir="finetuned_models/medgemma-4b-it-KTO",
+model.warnings_issued = {}
+
+training_args = KTOConfig(output_dir="finetuned_models/qwen3-8b-KTO",
                           eval_strategy="epoch",
-                          max_steps=10)
+                          report_to="none",
+                          logging_steps=1,
+                          max_steps=10,
+                          desirable_weight=15.0,
+                          undesirable_weight=1.0)
 trainer = KTOTrainer(model=model,
                      args=training_args, 
                      processing_class=tokenizer, 
